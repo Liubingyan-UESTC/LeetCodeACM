8115c9fb06ea71d43bdb168f45e1b4a34a53aa2f
liubingyan <lby_1026@outlook.com>
2026-08-10 23:32:25 +0800

    add longest_substr algorithm
    Signed-off-by: liubingyan <lby_1026@outlook.com>

--- longest_substr.py
+++ longest_substr.py
@@ -1,1 +1,37 @@
 # 无重复字符的最长子串
+import sys
+input = sys.stdin.readline
+MAX_N= 26
+
+def longest_substr(s : str) -> int:
+    len_s = len(s)
+    if len_s == 0:
+        return 0
+    l , r = 0 , 0
+    max_len = 0
+    cur_len = 0
+    count = [0 for _ in range(MAX_N)]
+    while r < len_s:
+        if count[ord(s[r]) - ord('a')] == 0:    # 当前字符没有出现过
+            count[ord(s[r]) - ord('a')] += 1
+            cur_len += 1
+            r += 1
+        else:
+            while s[l] != s[r]:
+                count[ord(s[l]) - ord('a')] -= 1
+                l += 1
+                cur_len -= 1
+            count[ord(s[l]) - ord('a')] -= 1
+            l += 1
+            cur_len -= 1
+        max_len = max(max_len , cur_len)
+    
+    return max_len
+
+def main():
+    s = input().strip()
+    print(longest_substr(s))
+
+
+
+
